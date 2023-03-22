5ee893ed81db4ef8657d4f833a290cf6d82fb92d
Ajju <abhamre50@gmail.com>
2023-03-22 16:04:20 +0530

    implemented useLocation Hook in React

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,5 @@
-import React, { useContext, useEffect } from "react";
-import NoteContext from "../context/notes/noteContext";
+import React from "react";
 
 export const About = () => {
-  const a = useContext(NoteContext);
-
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, []);
-
-  return (
-    <div>
-      About - This is about {a.state.name} and his roll num is {a.state.rollnum}
-    </div>
-  );
+  return <div>About - This is about page</div>;
 };
