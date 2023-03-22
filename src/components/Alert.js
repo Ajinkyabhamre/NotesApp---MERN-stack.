35326523f258fb31814d4a4bf8414d76477252b0
Ajju <abhamre50@gmail.com>
2023-03-22 20:25:55 +0530

    Adding AddNote component to iNotebook

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,7 +4,7 @@ export default function
 (props) {
   return (
     <div>
-      <div class="alert alert-primary" role="alert">
+      <div className="alert alert-primary" role="alert">
               { props.message}
       </div>
     </div>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,61 +4,40 @@ import NoteContext from "./noteContext";
 //In this file we are creating states which we wanted down the tree in any component without pasing props again and again
 
 const NoteState = (props) => {
-  const notesData = [
-    {
-      _id: "6419921fbbsc1275f68bfb751",
-      user: "64198cd38936a0c26f456e05",
-      title: "Daily Planet",
-      description: "Super-Man- Clark Kent is the Superman",
-      tag: "breaking-news",
-      date: "2023-03-21T11:16:47.199Z",
-      __v: 0,
-    },
-    {
-      _id: "641adb7s190cd98cb29d85d48",
-      user: "64198cd38936a0c26f456e05",
-      title: "Daily Planet",
-      description: "Super-Man- Clark Kent is the Superman",
-      tag: "breaking-news",
-      date: "2023-03-22T10:41:53.522Z",
-      __v: 0,
-    },
-    {
-      _id: "641adb7190csd98cbs29d85d4a",
-      user: "64198cd38936a0c26f456e05",
-      title: "Daily Planet",
-      description: "Super-Man- Clark Kent is the Superman",
-      tag: "breaking-news",
-      date: "2023-03-22T10:41:53.681Z",
-      __v: 0,
-    },
-    {
-      _id: "641asdb7190cd98cb29d85d4c",
-      user: "64198cd38936a0c26f456e05",
-      title: "Daily Planet",
-      description: "Super-Man- Clark Kent is the Superman",
-      tag: "breaking-news",
-      date: "2023-03-22T10:41:53.847Z",
-      __v: 0,
-    },
-    {
-      _id: "641adb719s0cd98cb29d85d4e",
-      user: "64198cd38936a0c26f456e05",
-      title: "Daily Planet",
-      description: "Super-Man- Clark Kent is the Superman",
-      tag: "breaking-news",
+
+
+  const [notes, setNotes] = useState([])
+
+  //Add a note
+  const addNote = (title, description, tag) => {
+    //TODO: API CALL
+    console.log("Adding a new note")
+    const note = {
+      _id: "123213",
+      user: "345656",
+      title: title,
+      description: description,
+      tag: tag,
       date: "2023-03-22T10:41:53.997Z",
       __v: 0,
-    },
-  ];
+    };
+    //concat return an array whereas push updates an array
+    setNotes(notes.concat(note))
+  
+}
+  //Delete a note
+    const deleteNote = () => {};
 
-  const [notes, setNotes] = useState(notesData)
+  //Edit a note
+    const editNote = () => {};
+
+
   return (
     //props.children are wrapped inside & value is passed as above state
     //{<-Javascript {<- object ->} ->}
 
     //instead of writing like this ->  {{state: state, update: update}}
-    <NoteContext.Provider value={{notes, setNotes}}>
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
       {props.children}
     </NoteContext.Provider>
   );
